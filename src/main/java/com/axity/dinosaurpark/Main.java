caf54fd12ee9d3ad5ffc86233c6f08ff2e3a70b1
Arturok2 <arturo_k2@hotmail.com>
2026-05-21 12:50:36 -0600

    Fase 0 - Estructura del proyecto

---
+++ src/main/java/com/axity/dinosaurpark/Main.java
@@ -0,0 +1,7 @@
+package com.axity.dinosaurpark;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Proyecto Dinosaur Park iniciado con éxito!");
+    }
+}
